525478ef45e9c661408edf7849a6edb9ff02852e
KrinklyDinkly <isaiah.rodriguez0207@gmail.com>
2024-01-19 12:34:16 -0800

    Update John Lemon

--- 3D Beginner/Assets/UnityTechnologies/Scripts/GameEnding.cs
+++ 3D Beginner/Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
     bool m_IsPlayerAtExit;
@@ -29,5 +30,10 @@ public class GameEnding : MonoBehaviour
     {
         m_Timer += Time.deltaTime;
         exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if(m_Timer > fadeDuration + displayImageDuration ) 
+        {
+            Application.Quit ();
+        }
     }
 }
